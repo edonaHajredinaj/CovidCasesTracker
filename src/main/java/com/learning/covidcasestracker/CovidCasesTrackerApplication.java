e817cb77638bfab41ad7797e1d4ec5a04f808343
Edona Hajredinaj <72093092+edonaHajredinaj@users.noreply.github.com>
2021-06-14 00:24:39 +0200

    refactor (#5)

--- src/main/java/com/learning/covidcasestracker/CovidCasesTrackerApplication.java
+++ src/main/java/com/learning/covidcasestracker/CovidCasesTrackerApplication.java
@@ -1,15 +1,11 @@
 package com.learning.covidcasestracker;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CovidCasesTrackerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CovidCasesTrackerApplication.class, args);
     }
-
-
 }
