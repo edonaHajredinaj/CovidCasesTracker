ebf8c7731823c67f24254d0f6b0afa92a9a6ab23
Edona Hajredinaj <72093092+edonaHajredinaj@users.noreply.github.com>
2021-06-13 22:13:01 +0200

    chore: added lombok library (#3)

--- src/main/java/com/learning/covidcasestracker/CovidCase.java
+++ src/main/java/com/learning/covidcasestracker/CovidCase.java
@@ -1,11 +1,18 @@
 package com.learning.covidcasestracker;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
 @Entity
 @Table
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CovidCase {
 
     @Id
@@ -31,117 +38,7 @@ public class CovidCase {
     private boolean recovered;
     private boolean deceased;
 
-    //no need for age to be a column in the database,
-    // age is calculated
-
-    protected CovidCase() {
-    }
-
-    protected CovidCase(String fullName,
-                        LocalDate birthday,
-                        String email,
-                        String casePeriod,
-                        String city,
-                        boolean underlyingDisease) {
-        this.fullName = fullName;
-        this.birthday = birthday;
-        this.email = email;
-        this.casePeriod = casePeriod;
-        this.city = city;
-        this.underlyingDisease = underlyingDisease;
-    }
-
-    public CovidCase(Integer patientID,
-                     String fullName,
-                     LocalDate birthday,
-                     String email,
-                     String casePeriod,
-                     String city,
-                     boolean underlyingDisease) {
-        PatientID = patientID;
-        this.fullName = fullName;
-        this.birthday = birthday;
-        this.email = email;
-        this.casePeriod = casePeriod;
-        this.city = city;
-        this.underlyingDisease = underlyingDisease;
-    }
-
-    public Integer getPatientID() {
-        return PatientID;
-    }
-
-    public void setPatientID(Integer patientID) {
-        PatientID = patientID;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
     public Integer getAge() {
         return Period.between(this.birthday, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCasePeriod() {
-        return casePeriod;
-    }
-
-    public void setCasePeriod(String casePeriod) {
-        this.casePeriod = casePeriod;
-    }
-
-    public boolean isUnderlyingDisease() {
-        return underlyingDisease;
-    }
-
-    public void setUnderlyingDisease(boolean underlyingDisease) {
-        this.underlyingDisease = underlyingDisease;
-    }
-
-    @Override
-    public String toString() {
-        return "CovidCase{" +
-                "PatientID=" + PatientID +
-                ", fullName='" + fullName + '\'' +
-                ", birthday=" + birthday +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                ", casePeriod='" + casePeriod + '\'' +
-                ", city='" + city + '\'' +
-                ", underlyingDisease=" + underlyingDisease +
-                '}';
     }
 }
